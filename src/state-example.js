cce249461898235e56b1afb9162e2a9df905dddb
Hung Tran <tmhung201088@gmail.com>
2018-02-09 17:57:34 +0800

    Bootstrap the example

---
+++ src/state-example.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class StateExample extends Component {
+    render() {
+        return (
+            <div>State Example</div>
+        );
+    }
+}
+
+export default StateExample;
